c4e61dfc814e5e745bc23c799663657fa217b29e
ARinger22 <gautamarpit1204@gmail.com>
2024-03-10 09:06:21 +0530

    updated files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import styled, { keyframes } from "styled-components";
+
 import Coordinate from "./components/Coordinate";
+
+const Heading = styled.span`
+  background: linear-gradient(90deg, #2563eb, #48bb78, #6366f1);
+  -webkit-background-clip: text;
+  color: transparent;
+`;
 
 function App() {
   return (
     <div>
       <h1 className="flex text-black font-bold text-4xl items-center justify-center h-20 bg-slate-600">
-        Drone-simulator
+        <Heading>Drone-simulator</Heading>
       </h1>
       <Coordinate />
     </div>
